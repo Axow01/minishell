455d59571dd0c4ad1d0f034c0c60dd5f00f19c50
TheMickQc <mickael.marcotte2004@gmail.com>
2023-10-13 08:33:29 -0400

    adding some dispach.c function

--- src/execution/dispach.c
+++ src/execution/dispach.c
@@ -1,8 +1,50 @@
 
 #include "../../includes/minishell.h"
 
+/// @brief This count the number of commands in the t_command's structure.
+/// @param cmd command struct.
+/// @return the number of command in it.
+int16_t	count_cmd(t_command *cmd)
+{
+	int			i;
+	t_command	*buf;
+
+	buf = cmd;
+	i = 0;
+	while (buf)
+	{
+		i++;
+		buf = buf->next;
+	}
+	return (i);
+}
+
+bool	simple_exec(t_command *cmd)
+{
+	pid_t	pid_fork;
+
+	pid_fork = fork();
+	if (pid_fork == -1)
+		return (false);
+	if (pid_fork == 0)
+	{
+		dup2(cmd->stdin_, STDIN_FILENO);
+		dup2(cmd->stdout_, STDOUT_FILENO);
+		execve(cmd->exec_cmd, cmd->cmd_argv, get_infos()->env);
+	}
+	waitpid(pid_fork, NULL, 0);
+	return (true);
+}
+
 void	execution_dispach(t_infos *infos)
 {
-	(void) infos;
+	t_command	*buf;
+
+	buf = &infos->cmd;
+	while (buf)
+	{
+		simple_exec(buf);
+		buf = buf->next;
+	}
 	return ;
 }
