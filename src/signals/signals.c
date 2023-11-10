df6379b70aadc9b343f1abf3d347c98c9316dd62
TheMickQc <mickael.marcotte2004@gmail.com>
2023-11-09 19:27:35 -0500

    jj

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -3,16 +3,12 @@
 
 void	sig_parent_handle(int signal)
 {
-	// char	nl_;
-
-	// nl_ = -1;
 	(void)signal;
-	// write(1, "\n", 1);
 	write(0, "\n", 1);
 	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
 	clean_cmd_struct(&get_infos()->cmd);
-	// rl_newline('0', 0);
 }
 
 void	ft_setup_signal(int type)
