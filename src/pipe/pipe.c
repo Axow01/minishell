92821cd1fd4a2c7d81aec9c6f7ca857ae2d7ecd8
TheMickQc <mickael.marcotte2004@gmail.com>
2023-11-15 15:15:52 -0500

    pipe test

--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -40,7 +40,10 @@ static void	run_fork(t_command *buf, t_infos *infos)
 		mms_kill(NULL, true, 1);
 	}
 	if (!check_cmd_valid(buf))
+	{
+		close_all_pipes(&infos->cmd);
 		mms_kill(NULL, true, 127);
+	}
 	if (buf->stdout_ != STDOUT_FILENO)
 		dup2(buf->stdout_, STDOUT_FILENO);
 	if (buf->stdin_ != STDIN_FILENO)
@@ -49,6 +52,7 @@ static void	run_fork(t_command *buf, t_infos *infos)
 	if (buf->is_builtin)
 	{
 		((t_builtin_ptr)buf->exec_cmd)(buf->arg_count, buf->cmd_argv, env);
+		close_all_pipes(&infos->cmd);
 		mms_kill(NULL, true, 1);
 	}
 	untrack_cmd(buf);
