5298b3c095a6937c2d28a193112666388c04291e
oboucher <oboucher@c1r6p2.42quebec.com>
2023-11-15 19:11:25 -0500

    try pars export

--- Makefile
+++ Makefile
@@ -5,8 +5,7 @@ LIBFT_A = libft.a
 
 #--- COMMAND VARIABLES ---#
 CC = gcc
-CFLAGS = -Wall -Wextra -Werror -g #-fsanitize=address
-#-fsanitize=address
+CFLAGS = -Wall -Wextra -Werror -g -fsanitize=address
 RM = rm -fd
 AR = ar rcs
 MK = mkdir -p
--- src/builtins/export/export.c
+++ src/builtins/export/export.c
@@ -65,13 +65,38 @@ int	get_env_index(char *key, char **env, size_t n)
 	return (i);
 }
 
+void	export_pars_err(int err)
+{
+	if (err != -1)
+		printf("export: not an identifier\n");
+}
+
+bool	export_pars(char *str, int *err)
+{
+	size_t	i;
+
+	i = 0;
+	if (str[i] && (str[i] == '_' || ft_isalpha(str[i])))
+		i++;
+	while (str[i] && (str[i] == '_' || ft_isalpha(str[i]) || ft_isalnum(str[i])))
+		i++;
+	if ((str[i] != '=' || i < ft_strlen(str)) && *err == -1)
+	{
+		*err = i;
+		return(false);
+	}
+	return (true);
+}
+
 void	ft_export(int ac, char **argv, char **env)
 {
 	char		**cpy_env;
 	t_key_val	*vk;
 	int			i;
 	int			ac_i;
+	int			err;
 
+	err = -1;
 	ac_i = 0;
 	cpy_env = env;
 	vk = NULL;
@@ -79,13 +104,17 @@ void	ft_export(int ac, char **argv, char **env)
 		print_double_char(env);
 	while (argv[++ac_i] && ac > 1)
 	{
-		cpy_env = copy_double_char(cpy_env, ft_length_d_char(cpy_env));
-		vk = export_get_key_val(argv[ac_i]);
-		i = get_env_index(vk->key, cpy_env, ft_strlen(vk->key));
-		if (i < 0)
-			cpy_env = create_new_variable(vk, cpy_env);
-		else
-			edit_variable(vk, cpy_env, i);
+		if (export_pars(argv[ac_i], &err))
+		{
+			cpy_env = copy_double_char(cpy_env, ft_length_d_char(cpy_env));
+			vk = export_get_key_val(argv[ac_i]);
+			i = get_env_index(vk->key, cpy_env, ft_strlen(vk->key));
+			if (i < 0)
+				cpy_env = create_new_variable(vk, cpy_env);
+			else
+				edit_variable(vk, cpy_env, i);
+		}
 	}
+	export_pars_err(err);
 	clean_vk(vk, cpy_env);
 }
